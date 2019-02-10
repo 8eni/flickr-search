892221da837f1ffcbf75e55dbcc507cb26dde640
Admin <admin@users-MacBook-Air.local>
2019-02-10 20:05:27 +0000

    Updates card styling, Moves styles from component to scss file

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -5,31 +5,28 @@ import { Component, Input } from '@angular/core';
   template: `
     <figure>
       <img class="mdc-image-list__image card-img-top"
-        [src]="result.thumbUrl"
-        alt="{{result.description}}">
+      [src]="result.thumbUrl"
+      alt="{{result.description}}">
       <figcaption class="card-body">
-        <h5 class="card-title font-weight-bold">{{ result.title }}</h5>
-        <h6 class="card-subtitle mb-2 text-muted" *ngIf="result.owner">
-          <i>by </i>{{result.owner}}
+        <h6 class="card-subtitle mb-2" >
+          <span class="owner" *ngIf="result.owner">{{result.owner}}</span>
+          <span *ngIf="result.dateTaken">{{result.dateTaken}}</span>
         </h6>
-        <p class="font-weight-light">{{result.dateTaken}}</p>
+        <h5 class="card-title">{{ result.title }}</h5>
         <hr>
-        <details>
-          <summary>Tags</summary>
-          <span *ngFor="let tag of result.tags" class="badge badge-pill badge-light">{{ tag }} </span>
-        </details>
-        <hr>
-        <a class="card-link" href="{{ result.link }}" target="_blank" *ngIf="result.link">View 
-          <span *ngIf="result.title">"{{ result.title }}"</span> on flickr
-        </a>
+        <div class="tag-wrapper">
+          <details>
+            <summary>View tags</summary>
+            <span *ngFor="let tag of result.tags" class="badge badge-light">{{ tag }} </span>
+          </details>
+          <a class="card-link" href="{{ result.link }}" target="_blank" *ngIf="result.link">
+            View on flickr
+          </a>
+        </div>
       </figcaption>
     </figure>
   `,
-  styles: [`
-    .badge-pill {
-      margin-right: 4px;
-    }
-  `]
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
 
